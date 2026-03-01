d65145c46c057cadee9ecb565b734ab79a2ce81f
nghias <108990223+nghias@users.noreply.github.com>
2026-03-01 17:37:56 +0700

    Them cau hinh GitHub Actions de deploy InfinityFree

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Xin chào từ Laravel Backend!'
+    ]);
+});
