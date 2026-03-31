72632336e0b6b98fed59356403afdbb337ab3eb1
binqhuy4444 <binqhuy6543210@gmail.com>
2026-03-31 17:50:48 +0700

    them api

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        "project" => "XDUDWEB - Backend API",
+        "status" => "running",
+        "apis" => [
+            "posts" => "/api/posts",
+            "users" => "/api/users",
+            "messages" => "/api/messages"
+        ]
+    ]);
 });
