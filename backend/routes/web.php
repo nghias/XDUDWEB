54ef2b16bd7c405529fe071eaacb63c3a044a021
binqhuy4444 <binqhuy6543210@gmail.com>
2026-03-31 17:58:31 +0700

    add api homepage

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,6 +6,7 @@ Route::get('/', function () {
     return response()->json([
         "project" => "XDUDWEB - Backend API",
         "status" => "running",
+        "server" => "Laravel on Render",
         "apis" => [
             "posts" => "/api/posts",
             "users" => "/api/users",
