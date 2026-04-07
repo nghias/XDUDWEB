c3732dd2404df3cc968285c849e9ca1ee07708b2
nghias <108990223+nghias@users.noreply.github.com>
2026-04-07 14:03:47 +0700

    Fix lỗi nội dung

--- frontend/src/page/general/ChangePassword.jsx
+++ frontend/src/page/general/ChangePassword.jsx
@@ -182,7 +182,7 @@ const ChangePassword = () => {
                     {/* Nút quay lại */}
                     <div className="text-center mt-4 pt-3 border-top">
                         <Link to="/" className="text-decoration-none text-secondary small fw-medium">
-                            <i className="bi bi-arrow-left me-1"></i> Về trang chủ
+                            <i className="bi bi-arrow-left me-1"></i> Quay lại
                         </Link>
                     </div>
                 </form>
