e92d89ab2afca169467ebf16196813485fbf6e51
nhat-nguyen2201 <phamvannhatnguyen@gmail.com>
2026-04-14 18:11:08 +0700

    update

--- backend/app/Models/GoiDichVu.php
+++ backend/app/Models/GoiDichVu.php
@@ -6,17 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class GoiDichVu extends Model
 {
-   
     protected $table = 'goi_dich_vu'; 
     
-   
     public $timestamps = false; 
 
-    // thêm/sửa dữ liệu
+    // Cho phép thêm/sửa các trường dữ liệu sau
     protected $fillable = [
         'ten_goi',
         'gia_tien',
         'thoi_han_ngay',
-        'muc_uu_tien'
+        'muc_uu_tien',
+        // --- 3 trường mới thêm vào ---
+        'so_tin_toi_da', 
+        'noi_bat',       
+        'mo_ta'          
     ];
 }
